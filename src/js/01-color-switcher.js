93afb70a71be06a5ac009797f437e57880e6f2b1
Antonina Bondar <tonya.bondar111@gmail.com>
2022-06-08 22:40:00 +0000

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+import "../css//01-color-switcher.css"
 
+
+const startButton = document.querySelector('button[data-start]')
+const stopButton = document.querySelector('button[data-stop]')
+let timerId = null;
+
+
+function onStartColorClick(){
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+      }, 1000);
+      if(timerId){
+          startButton.disabled = true
+          stopButton.disabled = false
+      }
+      
+}
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function onStopButtonClick(){
+    clearInterval(timerId);
+    if(timerId){
+        startButton.disabled = false
+        stopButton.disabled = true
+    }
+  }
+
+
+
+
+startButton.addEventListener('click', onStartColorClick)
+console.log(startButton)
+stopButton.addEventListener('click', onStopButtonClick)
