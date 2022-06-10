83092490eab2528f69bf3833b7ae8b9a8110c9c1
antonina94 <94193173+antonina94@users.noreply.github.com>
2022-06-10 22:39:38 +0000

    Deploying to gh-pages from @ antonina94/goit-js-hw-09@7b17b77364c14913f190c9a27cf4552fe5e222f2 🚀

---
+++ 01-color-switcher.5ec533e9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),d&&(t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(function(){clearInterval(d),d&&(t.disabled=!1,e.disabled=!0)}));
+//# sourceMappingURL=01-color-switcher.5ec533e9.js.map
